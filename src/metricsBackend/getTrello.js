93094aefba2df002518bcb4d7c39ae1a188063c0
Seal <Daniel.Seal@CenturyLink.com>
2018-04-11 11:25:25 -0500

    Reset load all and get trello

--- src/metricsBackend/getTrello.js
+++ src/metricsBackend/getTrello.js
@@ -4,11 +4,16 @@ var Trello = require("node-trello");
 // var cardID = [];
 var arr = [];
 var cardArr = [];
-var newStories = [];
+
+var systemID;
+var sprintID;
+var oldSprint;
 var sprintName;
-var sprintStartDate;
-var sprintEndDate;
-var allListResults;
+var oldSprint;
+
+var result;
+var stories;
+var fs = require('fs');
 
 exports.getList = (board) => {
     return new Promise((resolve, reject) => {
@@ -16,7 +21,7 @@ exports.getList = (board) => {
 
         //use function below to pull all of danny's boards with their id's
         //  t.get("/1/members/me/boards", { fields: "id,name"}, function(err, boards) {
-        //   console.log(boards); 
+        //   console.log(boards);
         //   console.log(err);
         // })
         //pulls board from trello associated with token above
@@ -51,6 +56,8 @@ exports.getAcceptedCards = () => {
 exports.getAllLists = (board) => {
     return new Promise((resolve, reject) => {
         var t = new Trello("68b96eeaad12e1cd6c22742f48289557", "c6910a2d151a944167560fd55fb977b862a32fc65d27025ee005bb02ea2610b5");
+        var newStories = [];
+
 
         //pulls lists from trello associated with board passed form front end
         t.get("/1/boards/" + board + "/lists", function (err, data) {
@@ -60,74 +67,76 @@ exports.getAllLists = (board) => {
             for (i = 0; i < data.length; i++) {
                 var listName = data[i].name.toLowerCase();
                 if (listName.includes('accepted')) {
-                    arr[j] = data[i];
-                    //listID = arr[j].id;
-                    //console.log(arr[2]);
-                    j++
+                    oldSprint = data[i]
+                    // for (var list in oldSprint) {
+                    systemID = oldSprint.idBoard;
+                    sprintID = oldSprint.id;
+                    var sprint = oldSprint.name.split(/[\s]+/);
+                    sprintName = "Sprint " + sprint[2];
+
+                    
+                    t.get("/1/lists/" + sprintID + "/cards", function (err, cardData) {
+                        if (err) throw err;
+
+                        result = cardData
+                        for (k = 0; k < result.length; k++) {
+                            var name = JSON.stringify(result[k].name);
+                            var pointsExp = /\(([^)]+)\)/;
+                            var point = pointsExp.exec(name);
+
+                            let story = {
+                                'story_id': result[k].id,
+                                'sprint_id': sprintID,
+                                'story_name': result[k].name,
+                                'story_points': point !== null ? point[1] : "no points"
+                            }
+
+                            newStories.push(story);
+                            // console.log(newStories)
+                        }
+                        
+                    });
+                   
+                    loadAll(newStories);
                 }
             }
-            resolve(arr);
+            // resolve(arr);
         });
     })
 }
 
-exports.getAllCards = (allAccepted) => {
+exports.getAllCards = (id) => {
     return new Promise((resolve, reject) => {
-        var allListResults = allAccepted
-        for (i = 0; i < allAccepted.length; i++) {
-            oldSprint = allAccepted[i]
-            systemID = oldSprint.idBoard;
-            sprintID = oldSprint.id;
-            var sprint = oldSprint.name.split(/[\s]+/);
-            sprintName = "Sprint " + sprint[2];
-
-            var t = new Trello("68b96eeaad12e1cd6c22742f48289557", "c6910a2d151a944167560fd55fb977b862a32fc65d27025ee005bb02ea2610b5");
-            t.get("/1/lists/" + sprintID + "/cards", function (err, cardData) {
-                if (err) throw err;
-                var story
-                result = cardData
-                for (k = 0; k < result.length; k++) {
-                    var name = JSON.stringify(result[k].name);
-                    var pointsExp = /\(([^)]+)\)/;
-                    var point = pointsExp.exec(name);
-                    let member = {
-                        'user': result[k].idMembers[0],
-                        'user_points': point !== null ? point[1] : "no points",
-                    }
-                    let story = {
-                        'story_id': result[k].id,
-                        'story_points': point !== null ? point[1] : "no points",
-                        'assigned _ to': member
-                    }
-                }
-                resolve(story)
-            });
-        }
+        var t = new Trello("68b96eeaad12e1cd6c22742f48289557", "c6910a2d151a944167560fd55fb977b862a32fc65d27025ee005bb02ea2610b5");
+        t.get("/1/lists/" + id + "/cards", function (err, cardData) {
+            if (err) throw err;
+            resolve(cardData);
+        });
     })
 }
-exports.writeAllCards = () => {
-    return new Promise((resolve, reject) => {
-        var sprint = listResult.name.split(/[\s]+/);
-        sprintName = "Sprint " + sprint[2];
-        sprintStartDate = sprint[3];
-        sprintEndDate = sprint[4];
-        let stories = {
-            // "system_id": systemID,
-            "sprint_id": sprintID,
-            "sprint_name": sprintName,
-            "sprint_start_date": sprintStartDate,
-            "sprint_end_date": sprintEndDate,
-            "stories: ": newStories
+
+function loadAll(t){
+    // var newStories = JSON.stringify(t);
+    var newStories = t;
+    console.log("2 " + t)
+    let stories = {
+        "system_id": systemID,
+        "sprint_id": sprintID,
+        "sprint_name": sprintName,
+        "table_name": "agile_story",
+        "table_rows": newStories
+    };
+
+    var sprintList = sprintName.split(":");
+    var sprint = sprintList[0];
+    var path = "Z:\\Metrics\\Exports\\NoJs_" + sprint + ".json";
+    fs.writeFile(path, JSON.stringify(stories, null, 4), (err) => {
+        if (err) {
+            console.error(err);
+            return;
         };
-        console.log(stories)
-        var sprint = sprintList[0];
-        var path = "A:\\Metrics\\Exports\\NoJs_" + sprint + ".json";
-        fs.writeFile(path, JSON.stringify(stories, null, 4), (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            };
-            console.log("File has been loggged to NoJs_" + sprint + ".json");
-        });
-    })
+        console.log("File has been loggged to NoJs_" + sprint + ".json");
+        sprintName = "";
+
+    });
 }
--- src/metricsBackend/load-old-trello-nojs.js
+++ src/metricsBackend/load-old-trello-nojs.js
@@ -2,29 +2,72 @@ var Trello = require("node-trello");
 var getTrello = require('./getTrello')
 var systemID;
 var sprintID;
-var allAccepted;
 var oldSprint;
 var sprintName;
 var oldSprint;
+var newStories = [];
 var result;
 var stories;
 var fs = require('fs');
 var t = new Trello("68b96eeaad12e1cd6c22742f48289557", "c6910a2d151a944167560fd55fb977b862a32fc65d27025ee005bb02ea2610b5");
 
 getTrello.getAllLists("59395d4f04c5416b8e7ed9a9").then((allAccepted) => {
-        getTrello.getAllCards(allAccepted).then((newStories) => {
-                getTrello.writeAllCards(newStories).then((newStories) => {
-                })
-        })
+        // for (i = 0; i < allAccepted.length; i++) {
+        //         oldSprint = allAccepted[i]
+        //         systemID = oldSprint.idBoard;
+        //         sprintID = oldSprint.id;
+        //         var sprint = oldSprint.name.split(/[\s]+/);
+        //         sprintName = "Sprint " + sprint[2];
+        //         //         let listInfo = {
+        //         //                 'sprint_id': sprintID,
+        //         //                 'system_id': systemID,
+        //         //                 'sprint_name': "Sprint " + sprint[2],
+        //         //                 'sprint_start_date': sprint[3],
+        //         //                 'sprint_end_date': sprint[4]
+        //         // }
+        //         t.get("/1/lists/" + sprintID + "/cards", function (err, cardData, allStories) {
+        //                  newStories = new Promise((resolve, reject) => {
+        //                         if (err) throw err;
+        //                         result = cardData
+        //                         for (k = 0; k < result.length; k++) {
+        //                                 var name = JSON.stringify(result[k].name);
+        //                                 var pointsExp = /\(([^)]+)\)/;
+        //                                 var point = pointsExp.exec(name);
+        //                                 // let member = {
+        //                                 //         'system_id': systemID,
+        //                                 //         'story_id': result[card].id,
+        //                                 //         'member_id': result[card].idMembers
+        //                                 // }
+        //                                 let story = {
+        //                                         'story_id': result[k].id,
+        //                                         'sprint_id': sprintID,
+        //                                         'story_name': result[k].name,
+        //                                         'story_points': point !== null ? point[1] : "no points"
+        //                                 }
+        //                         }
+        //                         resolve(story);
+        //                 });
+        //         });
+        //         let stories = {
+        //                 "system_id": systemID,
+        //                 "sprint_id": sprintID,
+        //                 "sprint_name": sprintName,
+        //                 "table_name": "agile_story",
+        //                 "table_rows": newStories
+        //         };
+        //         console.log(stories)
+        //         var sprintList = sprintName.split(":");
+        //         var sprint = sprintList[0];
+        //         var path = "A:\\Metrics\\Exports\\NoJs_" + sprint + ".json";
+        //         // fs.writeFile(path, JSON.stringify(stories, null, 4), (err) => {
+        //         //         if (err) {
+        //         //                 console.error(err);
+        //         //                 return;
+        //         //         };
+        //         //         console.log("File has been loggged to NoJs_" + sprint + ".json");
+        //         // });
+        // }
 })
-                        //         let listInfo = {
-                        //                 'sprint_id': sprintID,
-                        //                 'system_id': systemID,
-                        //                 'sprint_name': "Sprint " + sprint[2],
-                        //                 'sprint_start_date': sprint[3],
-                        //                 'sprint_end_date': sprint[4]
-                        // }
-
 
 
 
