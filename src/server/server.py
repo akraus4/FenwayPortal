a0c269188b8aae9658cc7ea18f7d69caaaac925e
Paul <dpaul@fenwaygroup.com>
2018-06-14 09:28:15 -0500

    Fixed Python Drop Down Issue

--- src/server/server.py
+++ src/server/server.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 db = MySQLdb.connect(
     host="52.55.14.143",  # your host
     user="fg_user_dev2",  # username
@@ -24,13 +25,17 @@ cur = db.cursor()
 
 # Create list of JSON objects for Select Team drop down in View Metrics Page
 
+
 @app.route("/findAllTeams")
 def findAllTeams():
     jsonList = []
     addObject = {}
     i = 0
+
     cur.execute("SELECT * FROM agile_system ORDER BY agile_system_name ASC")
+
     for row in cur.fetchall():
+
         addObject['agile_system_id'] = row[0]
         addObject['agile_system_name'] = row[1]
         addObject['agile_system_type'] = row[2]
@@ -38,9 +43,11 @@ def findAllTeams():
         jsonList.insert(i, addObject)
         addObject = {}
         i = i+1
+
     return json.dumps(jsonList)
 
 # Create list of JSON objects for Select sprint drop down in View Metrics Page
+
 
 @app.route("/findAllSprintsBySystem/<system_id>")
 def findAllSprintsBySystem(system_id):
@@ -49,6 +56,7 @@ def findAllSprintsBySystem(system_id):
     i = 0
     cur.execute("SELECT * FROM agile_sprint WHERE agile_system_id = '" +
                 system_id + "' ORDER BY agile_sprint_name ASC")
+
     for row in cur.fetchall():
         addObject['agile_sprint_id'] = row[0]
         addObject['agile_sprint_name'] = row[1]
@@ -59,13 +67,14 @@ def findAllSprintsBySystem(system_id):
         jsonList.insert(i, addObject)
         addObject = {}
         i = i+1
+
     return json.dumps(jsonList)
 
 # Create list of JSON objects for table in View Metrics Page
 
+
 @app.route("/findAllStoriesWithUsersBySprint/<sprint_id>")
 def findAllStoriesWithUsersBySprint(sprint_id):
-    # print(sprint_id)
     jsonList = []
     addObject = {}
     i = 0
@@ -175,7 +184,7 @@ def findTableData(table_name):
             addObject['name'] = row[4] + ' ' + row[5]
             jsonList.insert(i, addObject)
             addObject = {}
-            i = i+1
+            i=i+1
 
     elif table_name == "agile_system":
         jsonList = []
@@ -297,6 +306,22 @@ def findTableData(table_name):
 
     return json.dumps(jsonList)
 
+@app.route("/editTableDataWDailyhours/<wDailyhours_id>/<wTeam_member_id>/<work_date>/<hours>")
+def editTableDataWDailyHours(wDailyhours_id,wTeam_member_id,work_date,hours):
+    cur.execute("INSERT INTO work_dailyhours(work_dailyhours_id,work_team_member_id,work_date,hours)"
+                + "Values('" + wDailyhours_id + "','" + wTeam_member_id + "','" + work_date + "','" + hours + "')"
+                + "ON DUPLICATE KEY UPDATE work_team_member_id='" + wTeam_member_id + "', work_date='" + work_date + "', hours='" + hours + "'")
+    statementExecuted = "True"
+    return statementExecuted
+
+@app.route("/editTableDataASystem/<aSystem_id>/<aSystem_name>/<aSystem_type>/<wTeam_id>")
+def editTableDataASystem(aSystem_id,aSystem_name,aSystem_type,wTeam_id):
+    cur.execute("INSERT INTO agile_system(agile_system_id,agile_system_name,agile_system_type,work_team_id)"
+                + "Values('" + aSystem_id + "','" + aSystem_name + "','" + aSystem_type + "','" + wTeam_id + "')"
+                + "ON DUPLICATE KEY UPDATE agile_system_name='" + aSystem_name + "', agile_system_type='" + aSystem_type + "', work_team_id='" + wTeam_id + "'")
+    statementExecuted = "True"
+    return statementExecuted
+
 @app.route("/editTableDataWUser/<wUserId>/<fName>/<lName>/<email>")
 def editTableDataWUser(wUserId,fName,lName,email):
     cur.execute("INSERT INTO work_user(work_user_id,firstname,lastname,email)"
@@ -304,6 +329,7 @@ def editTableDataWUser(wUserId,fName,lName,email):
                 + "ON DUPLICATE KEY UPDATE work_user_id='" + wUserId + "', firstname='" + fName + "', lastname='" + lName + "', email='" + email + "'")
     statementExecuted = "True"
     return statementExecuted
+
 
 @app.route("/editTableDataWTeam/<wTeamId>/<wTeamName>/<pNameworkTeam>/<pName>")
 def editTableDataWTeam(wTeamId,wTeamName,pNameworkTeam,pName):
@@ -318,22 +344,6 @@ def editTableDataWTeamMember(wTeamMemberId,wTeamId,wUserId,eHours):
     cur.execute("INSERT INTO work_team_member(work_team_member_id,work_team_id,work_user_id,expected_hours)"
                 + "Values('" + wTeamMemberId + "','" + wTeamId + "','" + wUserId + "','" + eHours + "')"
                 + "ON DUPLICATE KEY UPDATE work_team_member_id='" + wTeamMemberId + "', work_team_id='" + wTeamId + "', work_user_id='" + wUserId + "', expected_hours='" + eHours + "'")
-    statementExecuted = "True"
-    return statementExecuted
-
-@app.route("/editTableDataWDailyhours/<wDailyhours_id>/<wTeam_member_id>/<work_date>/<hours>")
-def editTableDataWDailyHours(wDailyhours_id,wTeam_member_id,work_date,hours):
-    cur.execute("INSERT INTO work_dailyhours(work_dailyhours_id,work_team_member_id,work_date,hours)"
-                + "Values('" + wDailyhours_id + "','" + wTeam_member_id + "','" + work_date + "','" + hours + "')"
-                + "ON DUPLICATE KEY UPDATE work_team_member_id='" + wTeam_member_id + "', work_date='" + work_date + "', hours='" + hours + "'")
-    statementExecuted = "True"
-    return statementExecuted
-
-@app.route("/editTableDataASystem/<aSystem_id>/<aSystem_name>/<aSystem_type>/<wTeam_id>")
-def editTableDataASystem(aSystem_id,aSystem_name,aSystem_type,wTeam_id):
-    cur.execute("INSERT INTO agile_system(agile_system_id,agile_system_name,agile_system_type,work_team_id)"
-                + "Values('" + aSystem_id + "','" + aSystem_name + "','" + aSystem_type + "','" + wTeam_id + "')"
-                + "ON DUPLICATE KEY UPDATE agile_system_name='" + aSystem_name + "', agile_system_type='" + aSystem_type + "', work_team_id='" + wTeam_id + "'")
     statementExecuted = "True"
     return statementExecuted
 
@@ -358,6 +368,7 @@ def editTableDataAStory(aStoryId,aSprintId,sType,sStatus,sPoints):
     cur.execute("INSERT INTO agile_story(agile_story_id,agile_sprint_id,story_type,story_status,story_points)"
                 + "Values('" + aStoryId + "','" + aSprintId + "','" + sType + "','" + sStatus + "','" + sPoints + "')"
                 + "ON DUPLICATE KEY UPDATE agile_sprint_id='" + aSprintId + "',story_type='" + sType + "',story_status='" + sStatus + "',story_points='" + sPoints + "'")
+                # + "ON DUPLICATE KEY UPDATE agile_story_id='" + aStoryId + "', agile_sprint_id='" + aSprintId + "',story_type='" + sType + "',story_status='" + sStatus + "',story_points='" + sPoints + "'")
     statementExecuted = "True"
     return statementExecuted
 
@@ -374,7 +385,7 @@ def editTableDataASAgileSystemUser(aStoryAgileSystemUserId,aStoryId,aSystemUserI
 @app.route("/findDropDownData/<table_name>")
 def findDropDownData(table_name):
     
-    # print("Paramater: " + table_name)
+    print("Paramater: " + table_name)
 
 
     if table_name == "work_team":
@@ -390,15 +401,30 @@ def findDropDownData(table_name):
             i=i+1
     elif table_name == "work_team_member":
         jsonList = []
-        addObject = {}
-        i=0
-        cur.execute('SELECT work_team_id, work_team_name FROM work_team;')
+        addObjectTeam = {}
+        addObjectUser = {}
+        iTeam=0
+        iUser=0
+        jsonListTeam = []
+        jsonListUser = []
+        sqlTeam = 'SELECT work_team_id, work_team_name FROM work_team;'
+        sqlUser = 'SELECT work_user_id, firstname, lastname FROM work_user;'
+        cur.execute(sqlTeam)
         for row in cur.fetchall():
-            addObject['work_team_id'] = row[0]
-            addObject['work_team_name'] = row[1]
-            jsonList.insert(i, addObject)
-            addObject = {}
-            i=i+1
+            addObjectTeam['work_team_id'] = row[0]
+            addObjectTeam['work_team_name'] = row[1]
+            jsonListTeam.insert(iTeam, addObjectTeam)
+            addObjectTeam = {}
+            iTeam=iTeam+1
+        jsonList.insert(0, jsonListTeam)
+        cur.execute(sqlUser)
+        for row in cur.fetchall():
+            addObjectUser['work_user_id'] = row[0]
+            addObjectUser['name'] = row[1] + ' ' + row[2]
+            jsonListUser.insert(iUser, addObjectUser)
+            addObjectUser = {}
+            iUser= iUser+1
+        jsonList.insert(1, jsonListUser)
     elif table_name == "work_dailyhours":
         jsonList = []
         addObject = {}
@@ -443,7 +469,7 @@ def findDropDownData(table_name):
             addObjectSystem = {}
             iSystem=iSystem+1
         jsonList.insert(0, jsonListSystem)
-        # print(jsonList)
+        print(jsonList)
         # cur.execute(sqlMember)
         # for row in cur.fetchall():
         #     addObjectMember['work_team_member_id'] = row[0]
@@ -459,7 +485,7 @@ def findDropDownData(table_name):
             addObjectUser = {}
             iUser=iUser+1
         jsonList.insert(1, jsonListUser)
-        # print(jsonList)
+        print(jsonList)
     elif table_name == "agile_sprint":
         jsonList = []
         addObject = {}
@@ -485,15 +511,30 @@ def findDropDownData(table_name):
             i= i + 1
     elif table_name == "agile_story_agile_system_user":
         jsonList = []
-        addObject = {}
-        i=0
-        cur.execute('SELECT agile_story_id, agile_story_name FROM agile_story;')
+        addObjectStory = {}
+        addObjectSystem = {}
+        iStory=0
+        iSystem=0
+        jsonListStory = []
+        jsonListSystem = []
+        sqlStory = 'SELECT agile_story_id, agile_story_name FROM agile_story;'
+        sqlSystem = 'SELECT agile_system_user_id, agile_system_user_name FROM agile_system_user;'
+        cur.execute(sqlStory)
         for row in cur.fetchall():
-            addObject['agile_story_id'] = row[0]
-            addObject['agile_story_name'] = row[1]
-            jsonList.insert(i, addObject)
-            addObject = {}
-            i=i+1
+            addObjectStory['agile_story_id'] = row[0]
+            addObjectStory['agile_story_name'] = row[1]
+            jsonListStory.insert(iStory, addObjectStory)
+            addObjectStory = {}
+            iStory=iStory+1
+        jsonList.insert(0, jsonListStory)
+        cur.execute(sqlSystem)
+        for row in cur.fetchall():
+            addObjectSystem['agile_system_user_id'] = row[0]
+            addObjectSystem['agile_system_user_name'] = row[1]
+            jsonListSystem.insert(iSystem, addObjectSystem)
+            addObjectSystem = {}
+            iSystem=iSystem+1
+        jsonList.insert(1, jsonListSystem)
     return json.dumps(jsonList)
 
 
