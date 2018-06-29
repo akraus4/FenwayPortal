816161341051e5d9f0b2f659cc71e49678af4040
unknown <dpaul@fenwaygroup.com>
2018-06-29 08:58:42 -0500

    TypeORM is installed has entities and can pull data from the database.

--- src/server/TypeORM/src/index.ts
+++ src/server/TypeORM/src/index.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import {createConnection, ConnectionOptions} from "typeorm";
 import {agile_sprint} from "./entity/agile_sprint";
 import {agile_story_agile_system_user} from "./entity/agile_story_agile_system_user";
 import {agile_story} from "./entity/agile_story";
@@ -9,21 +9,27 @@ import {work_dailyhours} from "./entity/work_dailyhours";
 import {work_team_member} from "./entity/work_team_member";
 import {work_team} from "./entity/work_team";
 import {work_user} from "./entity/work_user";
+const config = require('../ormconfig.json')
 
 
-createConnection().then(async connection => {
-
-    const firstUser = await connection
-    .getRepository(work_user)
-    .createQueryBuilder("work_user")
-    .getRawMany();
-    console.log(work_user);
+createConnection({
+    type: "mysql",
+    host: "52.55.14.143",
+    port: 3306,
+    username: "fg_user_dev2",
+    password: "6UhjVvAgM_Jm",
+    database: "fg_metrics_dev2",
+    entities: [
+        __dirname + "/entity/*.ts"
+    ],
+    synchronize: true,
+    logging: false
+}).then(async connection => {
     
+    let userRepository = connection.getRepository(work_user);
 
-
-
-
+    let allUsers = await userRepository.find();
+    console.log("All users from the db: ", allUsers);
 
     
 }).catch(error => console.log(error));
-
