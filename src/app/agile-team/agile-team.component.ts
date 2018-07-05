806d37afe6b62f67bc56eaab7dfc3a0538668104
csalsbury <csalsbury@fenwaygroup.com>
2018-07-05 13:28:55 -0500

    Add/Edit Button Functionality

--- src/app/agile-team/agile-team.component.ts
+++ src/app/agile-team/agile-team.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DxSelectBoxModule, DxButtonModule, DxCheckBoxModule, DxTextBoxModule, DxDataGridModule } from 'devextreme-angular'
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-agile-team',
@@ -9,17 +10,45 @@ import { DxSelectBoxModule, DxButtonModule, DxCheckBoxModule, DxTextBoxModule, D
 export class AgileTeamComponent implements OnInit {
   systemSelectBoxDataSource = ["System 1", "System 2", "System 3"];
   teamSelectBoxDataSource = ["Team 1", "Team 2", "Team 3"];
+  buttonLbl:string;
+  readOnly;
+
   constructor() { }
 
   ngOnInit() {
+    $('#systemTextField').addClass('remove');
+    $('#typeTextField').addClass('remove');
+    $('#selectTeamDropDown').addClass('remove');
+    $('#activeCheckBox').addClass('remove');
+    $('#agileTeamSubmitCancelBtnContainer').addClass('remove');
+    this.readOnly = true;
   }
 
   addSystemButtonClicked() {
-    alert("The Add Button was clicked");
+    // alert("The Add Button was clicked");
+    this.buttonLbl = "Add";
+    this.readOnly = false;
+    $('#editSystemButton').addClass('remove');
+    $('#selectSystemDropDown').addClass('remove');
+    $('#systemTextField').removeClass('remove');
+    $('#typeTextField').removeClass('remove');
+    $('#selectTeamDropDown').removeClass('remove');
+    $('#activeCheckBox').removeClass('remove');
+    $('#agileTeamSubmitCancelBtnContainer').removeClass('remove');
+    
   }
 
   editSystemButtonClicked() {
-    alert("The Edit Button was clicked");
+    // alert("The Edit Button was clicked");
+    this.buttonLbl = "Update";
+    this.readOnly = false;
+    $('#addSystemButton').addClass('remove');
+    $('#selectSystemDropDown').addClass('remove');
+    $('#systemTextField').removeClass('remove');
+    $('#typeTextField').removeClass('remove');
+    $('#selectTeamDropDown').removeClass('remove');
+    $('#activeCheckBox').removeClass('remove');
+    $('#agileTeamSubmitCancelBtnContainer').removeClass('remove');
   }
 
   checkBoxToggled(e) {
@@ -36,7 +65,15 @@ export class AgileTeamComponent implements OnInit {
   }
 
   cancelButtonClicked() {
-    alert("The Cancel Button was clicked");
+    // alert("The Cancel Button was clicked"
+    this.readOnly = true;
+    $('#editSystemButton').removeClass('remove');
+    $('#addSystemButton').removeClass('remove');
+    $('#selectSystemDropDown').removeClass('remove');
+    $('#systemTextField').addClass('remove');
+    $('#selectTeamDropDown').addClass('remove');
+    $('#typeTextField').addClass('remove');
+    $('#activeCheckBox').addClass('remove');
+    $('#agileTeamSubmitCancelBtnContainer').addClass('remove');
   }
-
 }
