fe6b581b4a34dadd32ce9cd5baa335d5f1a821bf
unknown <dpaul@fenwaygroup.com>
2018-07-12 10:31:36 -0500

    TypeORM pulls and populates on the front end. I do have an error though. "buffer"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component, ElementRef, AfterViewInit } from '@angular/core';
-import { Routes, RouterModule, Router } from '@angular/router';
-import { SignInService } from '../app/services/sign-in.service';
 import { Injectable, Inject } from '@angular/core';
-declare const gapi: any;
-declare const signedIn: any;
+
 
 @Component({
   selector: 'app-root',
@@ -13,89 +10,13 @@ declare const signedIn: any;
 
 @Injectable()
 
-export class AppComponent implements AfterViewInit {
-  title = 'app';  SignInService:any;
-  googleUser;
+export class AppComponent {
 
-  private clientId: string = '409167539692-4eqnaq2jd1itl211gsgh3m2k7i02aefa.apps.googleusercontent.com';
 
-  private scope = [
-    'profile',
-    'email',
-    'https://www.googleapis.com/auth/plus.me',
-    'https://www.googleapis.com/auth/contacts.readonly',
-    'https://www.googleapis.com/auth/admin.directory.user.readonly'
-  ].join(' ');
 
-  public auth2: any;
-  public googleInit() {
-    let that = this;
-    gapi.load('auth2', function () {
-      that.auth2 = gapi.auth2.init({
-        client_id: that.clientId,
-        cookiepolicy: 'single_host_origin',
-        scope: that.scope
-        
-      });
-
-        that.attachSignin(that.element.nativeElement.firstChild);
-    });
-  }
-  public attachSignin(element) {
-    var profileImage;
-    let that = this;
-    this.auth2.attachClickHandler(element, {},
-      function (googleUser) {
-        let profile = googleUser.getBasicProfile();
-        // profileImage = profile.getImageUrl();
-        //this.setProfileImage(profile.getImageUrl());
-        console.log('Token || ' + googleUser.getAuthResponse().id_token);
-        console.log('ID: ' + profile.getId());
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-        // console.log('Profile: ' + profileImage);
-
-        
-      },
-      function (error) {
-        console.log(JSON.stringify(error, undefined, 2));
-      });
-      function onSignIn(googleUser) {
-        var id_token = googleUser.getAuthResponse().id_token
-
-        console.log(id_token) 
-        JSON.stringify(id_token)
-
-      }
-  }
-
-    authorizeUser(googleUser){
-      var id_token = googleUser.getAuthResponse().id_token
-      this.SignInService.authenticateUserWithServer(id_token) 
-      console.log(id_token) 
-      JSON.stringify(id_token)
-
-      if (id_token ==null){
-        this.router.navigate(['/signIn']);
-      }
-    };
-
+  constructor() {
   
-
-  // setProfileImage(p){
-  //   console.log('Profile: ' + JSON.stringify(p));
-  // }
-
-  constructor(private element: ElementRef,private router: Router) {
-    console.log('ElementRef: ', this.element);
   }
-
-  ngAfterViewInit() {
-    this.googleInit();
-
-  }
-
 
 
 }
--- src/server/TypeORM/src/index.ts
+++ src/server/TypeORM/src/index.ts
@@ -1,35 +1,89 @@
 import "reflect-metadata";
 import {createConnection, ConnectionOptions} from "typeorm";
-import {agile_sprint} from "./entity/agile_sprint";
-import {agile_story_agile_system_user} from "./entity/agile_story_agile_system_user";
-import {agile_story} from "./entity/agile_story";
-import {agile_system_user} from "./entity/agile_system_user";
-import {agile_system} from "./entity/agile_system";
-import {work_dailyhours} from "./entity/work_dailyhours";
-import {work_team_member} from "./entity/work_team_member";
-import {work_team} from "./entity/work_team";
-import {work_user} from "./entity/work_user";
+import * as express from 'express';
+import * as bodyParser from "body-parser";
+import {AgileSprint} from "./entities/agile_sprint";
+import {agile_story_agile_system_user} from "./entities/agile_story_agile_system_user";
+import {agile_story} from "./entities/agile_story";
+import {agile_system_user} from "./entities/agile_system_user";
+import {AgileSystem} from "./entities/agile_system";
+import "reflect-metadata";
+import {work_dailyhours} from "./entities/work_dailyhours";
+import {work_team_member} from "./entities/work_team_member";
+import {work_team} from "./entities/work_team";
+import {WorkUser} from "./entities/work_user";
+import { Request, Response } from "express";
+
+
 const config = require('../ormconfig.json')
 
 
+/**
+ * Controllers (route handlers).
+ */
+import * as empController from "./controllers/employee-controller";
+import * as wUserController from "./controllers/work_user-controller";
+import * as aSystemController from "./controllers/agile_system-controller";
+
+
+ 
+/**
+ * Create Express server.
+ */
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+/**
+ * Express configuration.
+ */
+app.set("port", process.env.PORT || 3000);
+ 
+
+
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get("port"), () => {
+    console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
+    console.log("  Press CTRL-C to stop\n");
+});
+ 
+
+/**
+ * Primary app routes.
+ */
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get("/GetAllEmployees", empController.getAllEmployees);
+app.get("/GetAllWorkUsers", wUserController.getAllWorkUsers);
+app.get("/getAllAgileSystems", aSystemController.getAllAgileSystems);
+
+
+
+/**
+ * Create connection to DB using configuration provided in 
+ * appconfig file.
+ */
 createConnection({
     type: "mysql",
-    host: "52.55.14.143",
+    host: "localhost",
     port: 3306,
-    username: "fg_user_dev2",
-    password: "6UhjVvAgM_Jm",
-    database: "fg_metrics_dev2",
+    username: "root",
+    password: "password",
+    database: "PORTAL",
     entities: [
-        __dirname + "/entity/*.ts"
+        __dirname + "/entity/*.ts", WorkUser, AgileSystem
     ],
     synchronize: true,
     logging: false
 }).then(async connection => {
-    
-    let userRepository = connection.getRepository(work_user);
 
-    let allUsers = await userRepository.find();
-    console.log("All users from the db: ", allUsers);
-
-    
 }).catch(error => console.log(error));
