0c3714aa841206a54cf388a854f6a8e711cac572
unknown <jfuller@fenwaygroup.com>
2018-07-16 14:56:04 -0500

    Made trello export configurable.

--- Trello/config/config.js
+++ Trello/config/config.js
@@ -2,7 +2,8 @@ var config = {
 
     teams: {
         noJsTeam:{
-            name: "No JS",
+            name: "No_JS",
+            team: "CTL_DA",
             board: "59395d4f04c5416b8e7ed9a9",
             key: "68b96eeaad12e1cd6c22742f48289557",
             token: "c6910a2d151a944167560fd55fb977b862a32fc65d27025ee005bb02ea2610b5",
@@ -11,14 +12,16 @@ var config = {
 
         lacTeam:{
             name: "LAC",
-            board: "59395d4f04c5416b8e7ed9a9",
+            team: "CTL_DA",
+            board: "5a7dd1e8a8fca8c51103c593",
             key: "68b96eeaad12e1cd6c22742f48289557",
             token: "c6910a2d151a944167560fd55fb977b862a32fc65d27025ee005bb02ea2610b5",
             acceptedColumn: 0
         },
 
         dataScrubbersTeam:{
-            name: "Data Scrubber",
+            name: "Data_Scrubber",
+            team: "CTL_DA",
             board: "5a2571728cac5d5f36ba5e5e",
             key: "68b96eeaad12e1cd6c22742f48289557",
             token: "c6910a2d151a944167560fd55fb977b862a32fc65d27025ee005bb02ea2610b5",
