9a2683c19e959cea4fd84c9d084303970d8300a7
unknown <jfuller@fenwaygroup.com>
2018-07-20 20:42:25 -0500

    Fixed Work Team, Work User, and Work Team Member entities.

--- src/server/TypeORM/src/entities/work_team_member.ts
+++ src/server/TypeORM/src/entities/work_team_member.ts
@@ -1,18 +1,27 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn} from "typeorm";
+import { WorkUser} from "../entities/work_user";
+import { WorkTeam} from "../entities/work_team";
+@Entity({name : "work_team_member"})
+export class WorkTeamMember {
 
-@Entity()
-export class work_team_member {
-
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn('uuid', {name : "work_team_member_id"})
     work_team_member_id: string;
 
-    @Column()
-    work_team_id: string;
+    // @Column()
+    // work_team_id: string;
 
-    @Column()
-    work_user_id: string;
+    // @Column()
+    // work_user_id: string;
 
-    @Column()
-    expected_hours: string;
+    @ManyToOne(type => WorkTeam)
+    @JoinColumn({name : "work_team_id"})
+    work_team: WorkTeam;
+
+    @ManyToOne(type => WorkUser)
+    @JoinColumn({name : "work_user_id"})
+    work_user: WorkUser;
+
+    // @Column("varchar", {name : "expected_hours"})
+    // expected_hours: string;
 
 }
